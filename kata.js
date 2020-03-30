f4d7f4c3b7e41fde922c20beb175133405b46fea
Kian <kian.info78@gmail.com>
2020-03-30 11:31:50 +1100

    DigToBin

--- kata.js
+++ kata.js
@@ -288,3 +288,18 @@ function DNAStrand(dna){
           .reduce(function(s,v){return s+v}) / n
         return ans%1 ? -1 : ans    
       }
+
+
+    //   implement a function that adds two numbers together and returns their sum in binary. 
+    //   The conversion can be done before, or after the addition.
+
+    function addBinary( a, b ){ 
+        var sum = a + b, binary = ''; 
+        while ( sum > 0 ) { binary = ( sum % 2 ) + binary; 
+        sum = Math.floor( sum / 2 ); } 
+        return binary; 
+        }
+
+    function addBinary( a, b ){  
+        return (a + b).toString(2);
+    }  
