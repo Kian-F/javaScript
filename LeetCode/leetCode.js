557ee4ad4bde9f0ae84d1a36081ec2daedc0a9c5
Kian-F <52603628+Kian-F@users.noreply.github.com>
2020-08-03 12:11:30 +1000

    twoSum

--- LeetCode/leetCode.js
+++ LeetCode/leetCode.js
@@ -29,3 +29,35 @@ var kidsWithCandies = function(candies, extraCandies) {
       }
       return result
   };
+
+
+  //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+
+//   Given an array of integers, return indices of the two numbers such that they add up to a specific target.
+
+// You may assume that each input would have exactly one solution, and you may not use the same element twice.
+
+// Example:
+
+// Given nums = [2, 7, 11, 15], target = 9,
+
+// Because nums[0] + nums[1] = 2 + 7 = 9,
+// return [0, 1].
+
+var twoSum = function(array, target) {
+    let myMap = new Map();
+       let result =[]
+       let indice;
+       for(let i=0;i<array.length; i++){
+           indice = target - array[i] 
+          if( myMap.has(indice)){
+              result.push(i)
+              result.push(indice)
+          } else  
+            (myMap.set(array[i]))  
+       } 
+       return result
+   };
+
+   
