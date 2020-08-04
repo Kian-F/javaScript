6a9545f1cb29ab0872c4e3b12ec15f6a7aac164c
Kian-F <52603628+Kian-F@users.noreply.github.com>
2020-08-04 23:11:05 +1000

    numJewelsInStones

--- LeetCode/leetCode.js
+++ LeetCode/leetCode.js
@@ -60,4 +60,32 @@ var twoSum = function(array, target) {
        return result
    };
 
-   
+   //////////////////////////////////////////////////////////////////////////////////////////////
+
+//    You're given strings J representing the types
+//  of stones that are jewels, and S representing the stones you have.  
+//  Each character in S is a type of stone you have.  You want to know how many of the stones you have are also jewels.
+
+// The letters in J are guaranteed distinct, and all characters in J and S are letters. Letters are case sensitive, 
+// so "a" is considered a different type of stone from "A".
+
+// Example 1:
+
+// Input: J = "aA", S = "aAAbbbb"
+// Output: 3
+
+var numJewelsInStones = function(J, S) {
+    let count =0;
+    let jewel = J.split('')
+    let stone = S.split('')
+    let myMap = new Map();
+     for(let i=0; i<stone.length; i++){
+         if(myMap.has(stone)){
+             count++
+         }else{
+             myMap.set(jewel, i)
+         }
+     }
+    
+    return count
+};
