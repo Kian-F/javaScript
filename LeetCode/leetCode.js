d140b331424c61d21b2d41c336bde49a524a026c
Kian-F <52603628+Kian-F@users.noreply.github.com>
2020-08-15 22:55:54 +1000

    singleNumber

--- LeetCode/leetCode.js
+++ LeetCode/leetCode.js
@@ -248,3 +248,32 @@ var deleteNode = function(node) {
   node.next = node.next.next
   
 };
+
+////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+// Given a non-empty array of integers, every element appears twice except for one. Find that single one.
+
+// Note:
+
+// Your algorithm should have a linear runtime complexity. Could you implement it without using extra memory?
+
+// Example 1:
+
+// Input: [2,2,1]
+// Output: 1
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var singleNumber = function(nums) {
+  let mySet = new Set();
+  for(let i=0; i< nums.length; i++){
+      if(mySet.has(nums[i])){
+         mySet.delete(nums[i])
+         }else{
+             mySet.add(nums[i])
+         }
+  }
+  
+  return Array.from(mySet)[0]
+};
