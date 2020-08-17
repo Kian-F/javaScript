7cbf4797dfe95fd8a44a4728262549ee7dd666a5
Kian-F <52603628+Kian-F@users.noreply.github.com>
2020-08-17 23:48:04 +1000

    sortedArrayToBST

--- LeetCode/leetCode.js
+++ LeetCode/leetCode.js
@@ -183,15 +183,13 @@ return its depth = 3.*/
  * @param {TreeNode} root
  * @return {number}
  */
-var maxDepth = function(root) {
-    if(!root){
-        return null;
-    }
-    let max = Math.max(maxDepth(root.left), maxDepth(root.right));
-    return max +1
-    
+var maxDepth = function (root) {
+  if (!root) {
+    return null;
+  }
+  let max = Math.max(maxDepth(root.left), maxDepth(root.right));
+  return max + 1;
 };
-
 
 //////////////////////////////////////////////////////////////////////////////////////////////////////////
 /*Given an array of size n, find the majority element. The majority element is the element that appears more than ⌊ n/2 ⌋ times.
@@ -203,20 +201,20 @@ Example 1:
 Input: [3,2,3]
 Output: 3*/
 
-var majorityElement = function(nums) {
-    let myMap = new Map();
-    let half = nums.length/2
-    for(num in nums){
-        if(!myMap.has(num)){
-            myMap.set(num, 1)
-        }else{
-            myMap.set(num, myMap.get(num)+1)
-        }
-        if(myMap.get(num) > half){
-            return num
-        }
+var majorityElement = function (nums) {
+  let myMap = new Map();
+  let half = nums.length / 2;
+  for (num in nums) {
+    if (!myMap.has(num)) {
+      myMap.set(num, 1);
+    } else {
+      myMap.set(num, myMap.get(num) + 1);
     }
-    return -1
+    if (myMap.get(num) > half) {
+      return num;
+    }
+  }
+  return -1;
 };
 
 ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
@@ -242,11 +240,9 @@ Explanation: You are given the second node with value 5, the linked list should 
  * @param {ListNode} node
  * @return {void} Do not return anything, modify node in-place instead.
  */
-var deleteNode = function(node) {
-    
-  node.val = node.next.val
-  node.next = node.next.next
-  
+var deleteNode = function (node) {
+  node.val = node.next.val;
+  node.next = node.next.next;
 };
 
 ////////////////////////////////////////////////////////////////////////////////////////////////////////////
@@ -265,15 +261,49 @@ var deleteNode = function(node) {
  * @param {number[]} nums
  * @return {number}
  */
-var singleNumber = function(nums) {
+var singleNumber = function (nums) {
   let mySet = new Set();
-  for(let i=0; i< nums.length; i++){
-      if(mySet.has(nums[i])){
-         mySet.delete(nums[i])
-         }else{
-             mySet.add(nums[i])
-         }
+  for (let i = 0; i < nums.length; i++) {
+    if (mySet.has(nums[i])) {
+      mySet.delete(nums[i]);
+    } else {
+      mySet.add(nums[i]);
+    }
   }
-  
-  return Array.from(mySet)[0]
+
+  return Array.from(mySet)[0];
+};
+
+////////////////////////////////////////////////////////////////////////////////////////////////////////////
+/*Given an array where elements are sorted in ascending order, convert it to a height balanced BST.
+
+For this problem, a height-balanced binary tree is defined as a binary tree in which 
+the depth of the two subtrees of every node never differ by more than 1.
+
+Example:
+
+Given the sorted array: [-10,-3,0,5,9],
+
+One possible answer is: [0,-3,9,-10,null,5], which represents the following height balanced BST:*/
+
+//Definition for a binary tree node.
+function TreeNode(val, left, right) {
+  this.val = val === undefined ? 0 : val;
+  this.left = left === undefined ? null : left;
+  this.right = right === undefined ? null : right;
+}
+
+/* @param {number[]} nums
+ * @return {TreeNode}
+ */
+var sortedArrayToBST = function (nums) {
+  if (nums.length === 1) return new TreeNode(nums[0]);
+  if (nums.length === 0) return null;
+  let mid = Math.floor(nums.length / 2);
+  let treeNode = new TreeNode(nums[mid]);
+  let left = nums.slice(0, mid);
+  treeNode.left = sortedArrayToBST(left);
+  let right = nums.slice(mid + 1, nums.length);
+  treeNode.right = sortedArrayToBST(right);
+  return treeNode;
 };
