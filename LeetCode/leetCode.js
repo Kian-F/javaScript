4831eb3b2162fcf4d462c7e8bb864b7fc8135176
Kian-F <52603628+Kian-F@users.noreply.github.com>
2020-09-10 22:21:29 +1000

    anagram

--- LeetCode/leetCode.js
+++ LeetCode/leetCode.js
@@ -517,3 +517,32 @@ const flatten = function( arr ){
 };
 
 flatten(["Hello", ["World", 42] ]);
+
+
+//////////////////////////////////////////////////////////////////////////////////////////
+/*Given two strings s and t , write a function to determine if t is an anagram of s.
+
+Example 1:
+
+Input: s = "anagram", t = "nagaram"
+Output: true
+Example 2:
+
+Input: s = "rat", t = "car"
+Output: false
+Note:
+You may assume the string contains only lowercase alphabets.*/
+/**
+ * @param {string} s
+ * @param {string} t
+ * @return {boolean}
+ */
+var isAnagram = function(s, t) {
+  let sToA = s.split('').sort().join('')
+ let tToA = t.split('').sort().join('')
+ if(sToA === tToA){
+   return true
+ }else{
+   return false
+ }
+};
