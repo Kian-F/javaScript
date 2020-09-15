b51bfc6973ffa2a9fbad8824e37581c4ec790cc7
Kian-F <52603628+Kian-F@users.noreply.github.com>
2020-09-15 14:43:53 +1000

    sieve

--- LeetCode/leetCode.js
+++ LeetCode/leetCode.js
@@ -574,3 +574,64 @@ var maxProfit = function(prices) {
   }
   return maxProfit;
 };
+
+
+////////////////////////////////////////////////////////////////////////////////////////////////////
+
+/*The Sieve of Eratosthenes
+The Sieve of Eratosthenes is a simple, ancient algorithm for finding all prime numbers up to any given limit.
+
+Create your range, starting at two and ending at the given limit.
+
+The algorithm consists of repeating the following over and over:
+
+take the next available unmarked number in your list (it is prime)
+remove all the multiples of that number for your list (they are not prime if they have factors other than themselves and 1) Repeat until you don't have any possible primes left in your range.
+When the algorithm terminates, all the numbers in the list that have not been removed are prime.
+
+Do this in Javascript!*/
+const sieve = {
+  list: [],
+  primes: [],
+  calcPrime: function(max){
+
+    // begin with all numbers as being unmarked
+    this.list = new Array(max).fill(true);
+
+    // Initially, we use i < max but if we use j = Math.pow(currentNum, 2)
+    // in the inner loop then there's no need to iterate until max
+    // For example, if your max was 100, there's no need to interate past 10,
+    // becuase the 10^2 is 100 and your inner loop won't run because j(100) is NOT < max(100)
+    for (let i = 2; i < Math.sqrt(max); i++) {
+      let currentNum = i;
+
+      // Initially, we started with j = currentNUm + 1
+      // BUT we can actually start at the square of currentNum
+      // For example, when we look at 3, 6 is ready FALSE because
+      // it is a multiple of 2, therefore we can start from 9 (which is the square of 3)
+      for (let j = Math.pow(currentNum, 2); j < max; j++) {
+        if (this.list[j] === true && j % currentNum === 0) {
+          this.list[j] = false;
+        }
+      } // inner loop
+    } // outer loop
+
+    // console.log(this.list);
+
+    // for (let i = 2; i < max; i++) {
+    //   if(this.list[i] === true){
+    //     this.primes.push(i);
+    //   }
+    // }
+
+    this.primes = this.list.reduce(function(array, elem, index){
+      if(index > 1 && elem === true){
+        array.push(index);
+      }
+      return array;
+    }, []); // set initial value as empty array
+
+
+    return this.primes;
+  }
+};
