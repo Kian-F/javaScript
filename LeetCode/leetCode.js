80b90224048bc388dac37e92339270b2b88fbfcd
Kian-F <52603628+Kian-F@users.noreply.github.com>
2020-09-17 23:29:03 +1000

    duplicate

--- LeetCode/leetCode.js
+++ LeetCode/leetCode.js
@@ -648,3 +648,34 @@ const sieve2 = function(limit) {
   }
   return primes;
 };
+
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+/*Given an array of integers, find if the array contains any duplicates.
+
+Your function should return true if any value appears at least twice in the array, and it should return false if every element is distinct.
+
+Example 1:
+
+Input: [1,2,3,1]
+Output: true*/
+
+/**
+ * @param {number[]} nums
+ * @return {boolean}
+ */
+var containsDuplicate = function(nums) {
+  let myMap = new Map();
+   
+   for(let i=0; i<nums.length; i++){
+       if(myMap.has(nums[i])){
+           return true
+          
+       }else {
+           myMap.set(nums[i])
+          
+       }
+   }
+   
+   return false
+   
+};
