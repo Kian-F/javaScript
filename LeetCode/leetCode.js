3211664b2bed91b4066bf8a0a9655518b40be6a6
Kian-F <52603628+Kian-F@users.noreply.github.com>
2020-09-23 23:34:44 +1000

    mergeSortedList

--- LeetCode/leetCode.js
+++ LeetCode/leetCode.js
@@ -707,3 +707,47 @@ var isAnagram = function(s, t) {
    return false
  }
 };
+
+////////////////////////////////////////////////////////////////////////
+/*Merge two sorted linked lists and return it as a new sorted list. 
+The new list should be made by splicing together the nodes of the first two lists.
+
+Example:
+
+Input: 1->2->4, 1->3->4
+Output: 1->1->2->3->4->4*/
+
+if (!l1 && !l2) return null;
+if (!l1) return l2;
+if (!l2) return l1;
+
+let resultList = new ListNode();
+let start = resultList;
+while (l1 || l2) {
+    if (l1 && l2) {
+        if (l1.val < l2.val) {
+            resultList.next = l1;
+            l1 = l1.next;
+        }
+        else {
+            resultList.next = l2;
+            l2 = l2.next;
+        }
+
+    }
+    else if (l1) {
+        resultList.next = l1;
+        l1 = l1.next;
+    }
+    else {
+        resultList.next = l2;
+        l2 = l2.next;
+    }
+    resultList = resultList.next;
+}
+
+const tmp = start.next;
+start = null;
+start = tmp;
+
+return start;
