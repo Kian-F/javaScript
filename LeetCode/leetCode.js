60a90e97335d7a2128b9da7387d901d92c916fe9
Kian-F <52603628+Kian-F@users.noreply.github.com>
2020-10-01 23:14:45 +1000

    missingNumber

--- LeetCode/leetCode.js
+++ LeetCode/leetCode.js
@@ -816,3 +816,26 @@ var maxProfit = function(prices) {
   }
   return maxProf   
 };
+
+/////////////////////////////////////////////////////////////////////////////////////////
+/*Given an array containing n distinct numbers taken from 0, 1, 2, ..., n, find the one that is missing from the array.
+
+Example 1:
+
+Input: [3,0,1]
+Output: 2
+Example 2:
+
+Input: [9,6,4,2,3,5,7,0,1]
+Output: 8*/
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var missingNumber = function(nums) {
+  let sum = nums.reduce(function(acc, cur){
+      return acc + cur
+})
+  let expectedSum = nums.length * (nums.length+1)/2
+      return expectedSum - sum
+};
