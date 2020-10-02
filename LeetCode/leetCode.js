56369d37057915b0dcaf735ebfba325027351b58
Kian-F <52603628+Kian-F@users.noreply.github.com>
2020-10-02 23:35:50 +1000

    intersect

--- LeetCode/leetCode.js
+++ LeetCode/leetCode.js
@@ -839,3 +839,38 @@ var missingNumber = function(nums) {
   let expectedSum = nums.length * (nums.length+1)/2
       return expectedSum - sum
 };
+
+
+/////////////////////////////////////////////////////////////
+// Given two arrays, write a function to compute their intersection.
+
+// Example 1:
+
+// Input: nums1 = [1,2,2,1], nums2 = [2,2]
+// Output: [2,2]
+// Example 2:
+
+// Input: nums1 = [4,9,5], nums2 = [9,4,9,8,4]
+// Output: [4,9]
+// Note:
+
+// Each element in the result should appear as many times as it shows in both arrays.
+// The result can be in any order.
+var intersect = function(nums1, nums2) {
+  let map = {}
+  let res = []
+  for(let i=0;i<nums1.length; i++){
+    if(map[nums1[i]]){
+      map[nums1[i]]++
+    }else{
+      map[nums1[i]] = 1
+    }
+  }
+  for(let j of nums2){
+     if(map[j]){
+       res.push(j)
+       map[j]--
+     }
+  }
+  return res
+};
