b5749dd4016e7191b1925f1859ad19e2c705a796
Kian-F <52603628+Kian-F@users.noreply.github.com>
2020-10-05 23:51:29 +1100

    maxSubArray

--- LeetCode/leetCode.js
+++ LeetCode/leetCode.js
@@ -874,3 +874,30 @@ var intersect = function(nums1, nums2) {
   }
   return res
 };
+
+////////////////////////////////////////////////////////////////////////
+  // Share
+  // Given an integer array nums, find the contiguous subarray (containing at least one number) which has the largest sum and return its sum.
+
+  // Follow up: If you have figured out the O(n) solution, try coding another solution using the divide and conquer approach, which is more subtle.
+
+  
+
+  // Example 1:
+
+  // Input: nums = [-2,1,-3,4,-1,2,1,-5,4]
+  // Output: 6
+  // Explanation: [4,-1,2,1] has the largest sum = 6.
+  // Example 2:
+
+  // Input: nums = [1]
+  // Output: 1
+  var maxSubArray = function(nums) {
+    let a1 = 0
+     let a2 = nums[0]
+     nums.forEach((i,a) => {
+       a1 = Math.max(i, a1 + i)
+       a2 = Math.max(a2, a1)
+     })
+     return a2
+   };
