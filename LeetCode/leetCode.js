cfe25b128f1e7046ba6a6af99134030712554e70
Kian-F <52603628+Kian-F@users.noreply.github.com>
2020-10-06 23:30:54 +1100

    twoSum

--- LeetCode/leetCode.js
+++ LeetCode/leetCode.js
@@ -901,3 +901,41 @@ var intersect = function(nums1, nums2) {
      })
      return a2
    };
+   
+   /////////////////////////////////////////////////////////////////////////////////////
+//    Given an array of integers nums and an integer target, return indices of the two numbers such that they add up to target.
+
+// You may assume that each input would have exactly one solution, and you may not use the same element twice.
+
+// You can return the answer in any order.
+
+ 
+
+// Example 1:
+
+// Input: nums = [2,7,11,15], target = 9
+// Output: [0,1]
+// Output: Because nums[0] + nums[1] == 9, we return [0, 1].
+// Example 2:
+
+// Input: nums = [3,2,4], target = 6
+// Output: [1,2]
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number[]}
+ */
+var twoSum = function(array, target) {
+  let myMap = new Map();
+     let result =[]
+     let indice;
+     for(let i=0;i<array.length; i++){
+         indice = target - array[i] 
+        if( myMap.has(indice)){
+            result.push(i)
+            result.push(indice)
+        } else  
+          (myMap.set(array[i]))  
+     } 
+     return result
+ };
