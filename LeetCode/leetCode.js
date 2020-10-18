83a6a3c11a73a0f737b7d5ddbcb2cb320c09bb5b
Kian-F <52603628+Kian-F@users.noreply.github.com>
2020-10-18 23:10:33 +1100

    hammingWeight

--- LeetCode/leetCode.js
+++ LeetCode/leetCode.js
@@ -997,3 +997,25 @@ var maxProfit = function(prices) {
   }
   return maxProfit;
 };
+
+/////////////////////////////////////////////////////////////////////////////////////
+
+// Write a function that takes an unsigned integer and returns the number of '1' bits it has (also known as the Hamming weight).
+
+// Note:
+
+// Note that in some languages such as Java, there is no unsigned integer type. In this case, the input will be given as a signed integer type. It should not affect your implementation, as the integer's internal binary representation is the same, whether it is signed or unsigned.
+// In Java, the compiler represents the signed integers using 2's complement notation. Therefore, in Example 3 above, the input represents the signed integer. -3.
+// Follow up: If this function is called many times, how would you optimize it?
+
+
+var hammingWeight = function(n) {
+  let arr = n.toString(2);
+  let count = 0;
+  for (let i = 0; i < arr.length; i++) {
+      if (arr[i] === "1") {
+          count++
+      }
+  }
+  return count;
+}
