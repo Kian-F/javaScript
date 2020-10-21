f15292f4b26b5d6d7bd970be49ac4011b73fb83b
Kian-F <52603628+Kian-F@users.noreply.github.com>
2020-10-21 23:14:53 +1100

    isHappy

--- LeetCode/leetCode.js
+++ LeetCode/leetCode.js
@@ -1019,3 +1019,40 @@ var hammingWeight = function(n) {
   }
   return count;
 }
+
+
+/////////////////////////////////////////////////////////////////////////////////////////////////
+/*Write an algorithm to determine if a number n is "happy".
+
+A happy number is a number defined by the following process: Starting with any positive integer, replace the number by the sum of the squares of its digits, and repeat the process until the number equals 1 (where it will stay), or it loops endlessly in a cycle which does not include 1. Those numbers for which this process ends in 1 are happy numbers.
+
+Return True if n is a happy number, and False if not.
+
+Example: 
+
+Input: 19
+Output: true
+Explanation: 
+12 + 92 = 82
+82 + 22 = 68
+62 + 82 = 100
+12 + 02 + 02 = 1 */
+
+var isHappy = function(n) {
+  let arr = [], sum = sumOfDigits(n);
+  while (!arr.includes(sum)) {
+      if (sum === 1) return true;
+      arr.push(sum);
+      sum = sumOfDigits(sum);
+  }
+  return false;
+};
+
+var sumOfDigits = function(num) {
+  let sum = 0;
+  while (num > 0) {
+      sum += Math.pow((num % 10), 2);
+      num = Math.floor(num / 10);
+  }
+  return sum;
+}
