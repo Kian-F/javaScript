e70ee81aec318b293024d2e7f09d777c1aa261ac
Kian-F <52603628+Kian-F@users.noreply.github.com>
2021-01-07 23:25:39 +1100

    happyNumber

--- LeetCode/leetCode.js
+++ LeetCode/leetCode.js
@@ -1101,3 +1101,60 @@ class ListNode {
     this.next = next;
   }
 }
+
+///////////////////////////////////////////////////////////////
+/*Write an algorithm to determine if a number n is happy.
+
+A happy number is a number defined by the following process:
+
+Starting with any positive integer, replace the number by the sum of the squares of its digits.
+Repeat the process until the number equals 1 (where it will stay), or it loops endlessly in a cycle which does not include 1.
+Those numbers for which this process ends in 1 are happy.
+Return true if n is a happy number, and false if not.
+
+ 
+
+Example 1:
+
+Input: n = 19
+Output: true
+Explanation:
+12 + 92 = 82
+82 + 22 = 68
+62 + 82 = 100
+12 + 02 + 02 = 1
+Example 2:
+
+Input: n = 2
+Output: false
+ 
+
+Constraints:
+
+1 <= n <= 231 - 1*/
+
+var getDigits = (num) => {
+  const digits = [];
+  while (num > 0) {
+    digits.push(num % 10);
+    num = Math.floor(num / 10);
+  }
+  return digits;
+};
+
+var getSquaredTotal = (digits) => {
+  return digits.reduce((total, digit) => total + digit * digit, 0);
+};
+
+var isHappy = function(n) {
+  const seen = new Set();
+  while (n !== 1) {
+    let digits = getDigits(n);
+    n = getSquaredTotal(digits);
+    if (seen.has(n)) {
+      return false;
+    }
+    seen.add(n);
+  }
+  return true;
+};
